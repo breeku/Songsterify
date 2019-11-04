a3a7b606d329950a7afdf7f8bc4fa6f507780364
breeku <bregfi@hotmail.com>
2019-11-04 20:50:50 +0200

    fix for heroku

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.use("/api/auth", authRouter)
 app.use("/api/spotify", spotifyRouter)
 app.use("/api/songsterr", songsterrRouter)
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const PORT = process.env.PORT
